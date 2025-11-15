8d8394e02cb301987dd4e3872f9e71598b0f387d
ynubhav <anubhavdixit35@gmail.com>
2025-11-15 23:44:59 +0530

    refactor: change projectId type to String in APIKey schema

--- backend/src/models/Apikey.model.js
+++ backend/src/models/Apikey.model.js
@@ -1,14 +1,18 @@
 import mongoose from "mongoose";
 
 const APIKeySchema = new mongoose.Schema({
-  projectId: { type: mongoose.Schema.Types.ObjectId, ref: "APIProject", required: true },
-  key: { type: String, required: true, unique: true },
+  projectId: { type: String, required: true },
+  key: { type: String, required: true, unique: true }, // should be hashed
   label: { type: String }, // e.g. "Production", "Testing"
-
+  keystatus: {
+    type: String,
+    enum: ["active", "disabled", "deleted"],
+    default: "active",
+  },
   lastUsed: { type: Date },
   createdAt: { type: Date, default: Date.now },
 });
 
 const APIKey = mongoose.model("APIKey", APIKeySchema);
 
-export { APIKey }
+export { APIKey };
