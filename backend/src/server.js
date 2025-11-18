25d00e71ac3964fd5332e7ef4757e3de53a6561b
ynubhav <anubhavdixit35@gmail.com>
2025-11-19 02:44:10 +0530

    style: format code for consistency and readability in server.js

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,25 +1,27 @@
-import express from 'express';
-import cors from 'cors';
-import { config } from 'dotenv';
-import { connectdb } from './configs/connectdb.js';
-import { appRouter } from './routes/index.js';
-import { gatewayRouter } from './routes/gateway.route.js';
-import create_routes_cache from './cache/create-routes-cache.js';
+import express from "express";
+import cors from "cors";
+import { config } from "dotenv";
+import { connectdb } from "./configs/connectdb.js";
+import { appRouter } from "./routes/index.js";
+import { gatewayRouter } from "./routes/gateway.route.js";
+import create_routes_cache from "./cache/create-routes-cache.js";
 
 config();
 
-const app=express();
+const app = express();
 
 await connectdb();
 await create_routes_cache();
 
 app.use(cors());
 app.use(express.json());
-app.use('/api',appRouter)      // this backend's apis
-app.use('/r',gatewayRouter)   // gateway for apis
+app.use("/api", appRouter); // this backend's apis
+app.use("/r", gatewayRouter); // gateway for apis
 
-app.use(async(req,res)=>{
-    res.json({message:"hello from api server"});
-})
+app.use(async (req, res) => {
+  res.json({ message: "hello from api server" });
+});
 
-app.listen(process.env.SERVER_PORT,()=>{console.log(`server running on port ${process.env.SERVER_PORT}`)});
+app.listen(process.env.SERVER_PORT, () => {
+  console.log(`server running on port ${process.env.SERVER_PORT}`);
+});
