0ac59068d9a937a628d2a82e5a335d5f4a9a1cba
ynubhav <anubhavdixit35@gmail.com>
2025-11-28 01:15:08 +0530

    fix: add emoji to server running log for better visibility

--- backend/src/server.js
+++ backend/src/server.js
@@ -23,5 +23,5 @@ app.use(async (req, res) => {
 });
 
 app.listen(process.env.SERVER_PORT, () => {
-  console.log(`server running on port ${process.env.SERVER_PORT}`);
+  console.log(`✅ server running on port ${process.env.SERVER_PORT}`);
 });
