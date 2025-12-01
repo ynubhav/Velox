1c0fcd2c50858858918ebee4b068bfc1552cac7b
ynubhav <anubhavdixit35@gmail.com>
2025-12-02 01:10:39 +0530

    feat: add Page component with WavyBackgroundtop integration

---
+++ frontend/app/getting-started/page.tsx
@@ -0,0 +1,10 @@
+import { WavyBackgroundtop } from "@/components/landing/home-wavy";
+
+export default function Page() {
+  return (
+    <div className="w-full">
+      <WavyBackgroundtop />
+    </div>
+  );
+}
+
