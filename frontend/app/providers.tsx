79aee1febce55a2334574814eeea56c6076460fe
ynubhav <anubhavdixit35@gmail.com>
2026-01-15 00:38:28 +0530

    feat: enhance Providers component with refetching options and Toaster positioning

--- frontend/app/providers.tsx
+++ frontend/app/providers.tsx
@@ -1,13 +1,13 @@
-'use client'
+"use client";
 import { SessionProvider } from "next-auth/react";
 import React from "react";
 import { Toaster } from "sonner";
 
-export default function Providers({children}:{children:React.ReactNode}){
-    return(
-        <SessionProvider>
-            <Toaster/>
-                {children}
-        </SessionProvider>
-    )
+export default function Providers({ children }: { children: React.ReactNode }) {
+  return (
+    <SessionProvider refetchInterval={60} refetchOnWindowFocus={true} >
+      <Toaster position="top-center" /> 
+        {children}
+    </SessionProvider>
+  );
 }
